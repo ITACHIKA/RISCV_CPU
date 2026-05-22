ca47d71a8358d680490f0eba5bafd8bd1f979ad7
ITACHIKA <iamwojiaoTM@outlook.com>
2026-05-21 21:59:38 -0700

    uses separate asm file

--- src/asm_gen.py
+++ src/asm_gen.py
@@ -274,18 +274,9 @@ def encode_line(line):
     raise ValueError(f"Unsupported instruction: {line}")
 
 # ===== main =====
-asm = [
-    "addi x1, x0, 5",
-    "addi x2, x0, 4",
-    "addi x7, x0, -1",
-    "sw x1, 0(x2)",        # Store x1 to memory at x2+0
-    "lw x3, 0(x2)",        # Load from memory at x2+0 into x3
-    "sh x1, 6(x2)",        # Store halfword x1 to x2+4
-    "lh x4, 6(x2)",        # Load halfword from x2+4 into x4
-    "sb x7, 9(x2)",        # Store byte x1 to x2+8
-    "lbu x5, 9(x2)",        # Load byte from x2+8 into x5
-    "lb x6, 9(x2)",      # Load byte unsigned from x2+8 into x6
-]
+asm= ""
+with open("asm.s") as f:
+    asm = f.readlines()
 
 with open("instr_rom.mem", "w") as f:
     for line in asm:
