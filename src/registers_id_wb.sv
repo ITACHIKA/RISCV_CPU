f00c3834a5c2bbae58622541baf0cd6f6dbe0d12
ITACHIKA <iamwojiaoTM@outlook.com>
2026-08-02 19:29:16 -0700

    adds WB->ID rd forwarding mechanism

--- src/registers_id_wb.sv
+++ src/registers_id_wb.sv
@@ -31,7 +31,36 @@ always_ff @(posedge clk) begin
 end
 
 // always read 0 from x0
-assign rs1_data = (rs1_addr == 0) ? 32'd0 : registers[rs1_addr];
-assign rs2_data = (rs2_addr == 0) ? 32'd0 : registers[rs2_addr];
+// assign rs1_data = (rs1_addr == 0) ? 32'd0 : registers[rs1_addr];
+// assign rs2_data = (rs2_addr == 0) ? 32'd0 : registers[rs2_addr];
+
+// we need a forwarding machanism to forward data from WB stage to ID decode
+// without forwarding, WB updates register at rising edge, but ID also captures reg at rising edge
+// So ID will capture old value of register
+always_comb begin
+    if(rs1_addr == 5'd0) begin
+        rs1_data = 32'd0;
+    end
+    else begin
+        if(rs1_addr == rd_addr && rd_we && rd_addr != 5'd0) begin
+            rs1_data = rd_data;
+        end
+        else begin
+            rs1_data = registers[rs1_addr];
+        end
+    end
+
+    if(rs2_addr == 5'd0) begin
+        rs2_data = 32'd0;
+    end
+    else begin
+        if(rs2_addr == rd_addr && rd_we && rd_addr != 5'd0) begin
+            rs2_data = rd_data;
+        end
+        else begin
+            rs2_data = registers[rs2_addr];
+        end
+    end
+end
 
 endmodule
