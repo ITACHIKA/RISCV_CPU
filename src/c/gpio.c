96439ec64f736536f2be44a01820a3304277532a
ITACHIKA <iamwojiaoTM@outlook.com>
2026-08-30 15:39:05 -0700

    uses io header to separate HW register definition

--- src/c/gpio.c
+++ src/c/gpio.c
@@ -1,17 +1,13 @@
-#define LED_ADDR 0x40000000u
-#define BTN_ADDR 0x40000004u
+#include "riscv/io.h"
 
 int main() {
-    volatile unsigned int *led = (unsigned int *)LED_ADDR;
-    volatile unsigned int *btn = (unsigned int *)BTN_ADDR;
-    unsigned int led_state = 0;// init once
     while(1)
     {
-        while((*btn & 0x00000001u) == 0u) { // unpressed
+        while((BTN & 0x00000001u) == 0u) { // unpressed
             // wait
         }
-        *led ^= 0x00000001u; // toggle LED state
-        while((*btn & 0x00000001u) != 0u) { // pressed
+        LED ^= 0x00000001u; // toggle LED state
+        while((BTN & 0x00000001u) != 0u) { // pressed
             // wait
         }
     }
---
+++ src/c/riscv/io.h
@@ -0,0 +1,2 @@
+#define LED *(volatile unsigned int *)0x40000000u
+#define BTN *(volatile unsigned int *)0x40000004u
